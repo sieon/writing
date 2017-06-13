64553587f42152e2772d48224b5fd08cd07268f6
sieon <sieon@outlook.com>
2017-06-13 13:52:27 +0800

    更新bootstrap，调整文件结构

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,14 +4,14 @@
  */
 function lean_scripts() {
 
-  wp_enqueue_style( 'bootstrap', THEME_URI . '/assets/css/bootstrap/bootstrap.css');
-	wp_enqueue_style( 'font-awesome', THEME_URI . '/assets/css/font-awesome.min.css');
-  wp_enqueue_style( 'main', THEME_URI . '/assets/css/main.css');
+  wp_enqueue_style( 'lean-bootstrap', THEME_URI . '/assets/css/bootstrap.css'); //20170613
+	wp_enqueue_style( 'lean-font-awesome', THEME_URI . '/assets/css/font-awesome.min.css');
+  wp_enqueue_style( 'lean-main', THEME_URI . '/assets/css/main.css');
 
-	wp_enqueue_script( 'lean-jquery', THEME_URI . '/assets/js/jquery-3.2.1.min.js', array(), '20170609', true );
-  wp_enqueue_script( 'lean-popper', THEME_URI . '/assets/js/popper.min.js', array(), '20170416', true );
-	wp_enqueue_script( 'lean-tether', THEME_URI . '/tether.min.js', array(), '20170609', true );
-	wp_enqueue_script( 'lean-bootstrap', THEME_URI . '/assets/js/bootstrap.min.js', array(), '20170417', true );
+	wp_enqueue_script( 'lean-jquery', THEME_URI . '/assets/js/jquery.js', array(), '20170613', true );
+  wp_enqueue_script( 'lean-popper', THEME_URI . '/assets/js/popper.js', array(), '20170416', true );
+	wp_enqueue_script( 'lean-tether', THEME_URI . '/tether.js', array(), '20170609', true );
+	wp_enqueue_script( 'lean-bootstrap', THEME_URI . '/assets/js/bootstrap.js', array(), '20170613', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
