dbc1360224f1a317cc6aad74428bcb119b821306
sieon <sieon@outlook.com>
2017-06-23 22:52:23 +0800

    修改日期格式和清理代码

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -189,42 +189,12 @@ function lean_the_post_navigation() {
 endif;
 
 /**
- * WordPress 修改时间的显示格式为几天前
- * https://www.wpdaxue.com/time-ago.html
+ * 固定时间格式，只显示日期
  */
+
 function lean_filter_time() {
- global $post ;
- $to = time();
- $from = get_the_time('U') ;
- $diff = (int) abs($to - $from);
- if ($diff <= 3600) {
-   $mins = round($diff / 60);
-   if ($mins <= 1) {
-     $mins = 1;
-   }
-   $time = sprintf(_n('%s 分钟', '%s 分钟', $mins), $mins) . __( '前' , 'lean' );
- }
- else if (($diff <= 86400) && ($diff > 3600)) {
-   $hours = round($diff / 3600);
-   if ($hours <= 1) {
-     $hours = 1;
-   }
-   $time = sprintf(_n('%s 小时', '%s 小时', $hours), $hours) . __( '前' , 'lean' );
- }
- elseif ($diff >= 86400) {
-   $days = round($diff / 86400);
-   if ($days <= 1) {
-     $days = 1;
-     $time = sprintf(_n('%s 天', '%s 天', $days), $days) . __( '前' , 'lean' );
-   }
-   elseif( $days > 29){
-     $time = get_the_time(get_option('date_format'));
-   }
-   else{
-     $time = sprintf(_n('%s 天', '%s 天', $days), $days) . __( '前' , 'lean' );
-   }
- }
- return $time;
+  global $post ;
+  echo get_the_time(get_option('date_format'));
 }
 add_filter('the_time','lean_filter_time');
 
@@ -275,94 +245,6 @@ function lean_entry_meta() {
 }
 endif;
 
-
-if ( ! function_exists( 'lean_entry_meta' ) ) :
-/**
- * Prints HTML with meta information for the categories, tags.
- *
- * @since Twenty Fifteen 1.0
- */
-function twentyfifteen_entry_meta() {
-	if ( is_sticky() && is_home() && ! is_paged() ) {
-		printf( '<span class="sticky-post">%s</span>', __( 'Featured', 'twentyfifteen' ) );
-	}
-
-	$format = get_post_format();
-	if ( current_theme_supports( 'post-formats', $format ) ) {
-		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-			sprintf( '<span class="sr-only sr-only-focusable">%s </span>', _x( 'Format', 'Used before post format.', 'twentyfifteen' ) ),
-			esc_url( get_post_format_link( $format ) ),
-			get_post_format_string( $format )
-		);
-	}
-
-	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-		}
-
-		$time_string = sprintf( $time_string,
-			esc_attr( get_the_date( 'c' ) ),
-			get_the_date(),
-			esc_attr( get_the_modified_date( 'c' ) ),
-			get_the_modified_date()
-		);
-
-		printf( '<span class="posted-on"><span class="sr-only sr-only-focusable">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
-			_x( 'Posted on', 'Used before publish date.', 'twentyfifteen' ),
-			esc_url( get_permalink() ),
-			$time_string
-		);
-	}
-
-	if ( 'post' == get_post_type() ) {
-		if ( is_singular() || is_multi_author() ) {
-			printf( '<span class="byline"><span class="author vcard"><span class="sr-only sr-only-focusable">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				_x( 'Author', 'Used before post author name.', 'twentyfifteen' ),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-				get_the_author()
-			);
-		}
-
-		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfifteen' ) );
-		if ( $categories_list && twentyfifteen_categorized_blog() ) {
-			printf( '<span class="cat-links"><span class="sr-only sr-only-focusable">%1$s </span>%2$s</span>',
-				_x( 'Categories', 'Used before category names.', 'twentyfifteen' ),
-				$categories_list
-			);
-		}
-
-		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfifteen' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links"><span class="sr-only sr-only-focusable">%1$s </span>%2$s</span>',
-				_x( 'Tags', 'Used before tag names.', 'twentyfifteen' ),
-				$tags_list
-			);
-		}
-	}
-
-	if ( is_attachment() && wp_attachment_is_image() ) {
-		// Retrieve attachment metadata.
-		$metadata = wp_get_attachment_metadata();
-
-		printf( '<span class="full-size-link"><span class="sr-only sr-only-focusable">%1$s </span><a href="%2$s">%3$s &times; %4$s</a></span>',
-			_x( 'Full size', 'Used before full size attachment link.', 'twentyfifteen' ),
-			esc_url( wp_get_attachment_url() ),
-			$metadata['width'],
-			$metadata['height']
-		);
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		/* translators: %s: post title */
-		comments_popup_link( sprintf( __( 'Leave a comment<span class="sr-only sr-only-focusable"> on %s</span>', 'twentyfifteen' ), get_the_title() ) );
-		echo '</span>';
-	}
-}
-endif;
 
 if ( ! function_exists( 'lean_the_archive_title' ) ) :
 /**
