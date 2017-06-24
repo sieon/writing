7815e8a7a917aa4df446d00bcbd80af3f950448c
sieon <sieon@outlook.com>
2017-06-24 18:30:28 +0800

    启用默认小工具：页面，清理代码

--- inc/widget.php
+++ inc/widget.php
@@ -6,11 +6,11 @@
  */
 
 /**
- * Remove Default Widgets
+ * 移除默认的小工具
  */
 function unregister_widgets() {
   unregister_widget( 'WP_Widget_RSS' );
-  unregister_widget( 'WP_Widget_Pages' );
+  //unregister_widget( 'WP_Widget_Pages' );
   unregister_widget( 'WP_Widget_Search' );
   unregister_widget( 'WP_Widget_Recent_Comments' );
   //unregister_widget( 'WP_Nav_Menu_Widget' );
@@ -26,6 +26,9 @@ function unregister_widgets() {
 
 add_action( 'widgets_init', 'unregister_widgets' );
 
+/**
+ * 注册边栏
+ */
 function lean_widgets_init() {
   register_sidebar( array(
   	'name'          => esc_html__( '全局边栏', 'lean' ),
@@ -36,50 +39,5 @@ function lean_widgets_init() {
   	'before_title'  => '<h4 class="widget-header">',
   	'after_title'   => '</h4>',
   ) );
-  // register_sidebar( array(
-  //   'name'          => esc_html__( '首页-文章列表 1', 'lean' ),
-  //   'id'            => 'home-block-1',
-  //   'description'   => '只有标题的列表。',
-  //   'before_widget' => '<div id="%1$s" class="widget %2$s">',
-  //   'after_widget'  => '</div>',
-  //   'before_title'  => '<h4 class="widget-header">',
-  // 	'after_title'   => '</h4>',
-  // ) );
-  // register_sidebar( array(
-  //   'name'          => esc_html__( '首页-文章列表 2', 'lean' ),
-  //   'id'            => 'home-block-2',
-  //   'description'   => '只有标题的列表。',
-  //   'before_widget' => '<div id="%1$s" class="widget %2$s">',
-  //   'after_widget'  => '</div>',
-  //   'before_title'  => '<h4 class="widget-header">',
-  // 	'after_title'   => '</h4>',
-  // ) );
-  // register_sidebar( array(
-  //   'name'          => esc_html__( '顶部广告', 'lean' ),
-  //   'id'            => 'home-ad-1',
-  //   'description'   => '这是顶部的长条广告。',
-  //   'before_widget' => '<div id="%1$s" class="widget %2$s">',
-  //   'after_widget'  => '</div>',
-  //   'before_title'  => '<h4 class="card-header widget-header">',
-  //   'after_title'   => '</h4>',
-  // ) );
-  // register_sidebar( array(
-  //   'name'          => esc_html__( '右侧产品', 'lean' ),
-  //   'id'            => 'home-ad-2',
-  //   'description'   => '',
-  //   'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-  //   'after_widget'  => '</aside>',
-  //   'before_title'  => '<h4 class="card-header widget-header">',
-  //   'after_title'   => '</h4>',
-  // ) );
-  // register_sidebar( array(
-  //   'name'          => esc_html__( '三个广告', 'lean' ),
-  //   'id'            => 'home-ad-3',
-  //   'description'   => '使用<a href="地址"><img src="图片地址" /></a>添加广告。如果是三个：<div class="row"><div class="col-md-3"><a href="地址"><img src="图片地址" /></a></div><div class="col-md-4"><a href="地址"><img src="图片地址" /></a></div><div class="col-md-5"><a href="地址"><img src="图片地址" /></a></div></div>',
-  //   'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-  //   'after_widget'  => '</aside>',
-  //   'before_title'  => '<h4 class="widget-header">',
-  //   'after_title'   => '</h4>',
-  // ) );
 }
 add_action( 'widgets_init', 'lean_widgets_init' );
