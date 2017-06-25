584b61bcd986375960cc9c325053fdf3fd818125
sieon <sieon@outlook.com>
2017-06-25 20:27:07 +0800

    最近评论样式

--- inc/widget.php
+++ inc/widget.php
@@ -12,12 +12,12 @@ function unregister_widgets() {
   unregister_widget( 'WP_Widget_RSS' );
   //unregister_widget( 'WP_Widget_Pages' );
   unregister_widget( 'WP_Widget_Search' );
-  unregister_widget( 'WP_Widget_Recent_Comments' );
+  //unregister_widget( 'WP_Widget_Recent_Comments' );
   //unregister_widget( 'WP_Nav_Menu_Widget' );
   //unregister_widget( 'WP_Widget_Archives' );
   //unregister_widget( 'WP_Widget_Calendar' );
   //unregister_widget( 'WP_Widget_Categories' );
-  unregister_widget( 'WP_Widget_Links' );
+  //unregister_widget( 'WP_Widget_Links' );
   //unregister_widget( 'WP_Widget_Meta' );
   //unregister_widget( 'WP_Widget_Recent_Posts' );
   //unregister_widget( 'WP_Widget_Tag_Cloud' );
