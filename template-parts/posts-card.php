cf497d46d454d629b9d340c8878f51257b26bada
sieon <sieon@outlook.com>
2017-06-30 15:19:02 +0800

    作者页面：第二次

--- template-parts/posts-card.php
+++ template-parts/posts-card.php
@@ -7,7 +7,7 @@
 
 <?php if(has_post_thumbnail()) : ?>
 
-<div class="col-lg-4">
+<div class="col-lg-3">
 
 	<div class="card">
 		<a class="entry-img" href="<?php the_permalink(); ?>">
@@ -16,15 +16,7 @@
 	    ?>
 	  </a>
 		<div class="card-block">
-			<?php the_title( sprintf( '<h2 class="card-title entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-			<?php if ( get_theme_mod( 'posts_list_excerpt')==yes ) { ?>
-				<div class="entry-excerpt hidden-sm-down">
-					<?php the_excerpt(); ?>
-				</div>
-			<?php } else {
-				echo '';
-			 } ?>
+			<?php the_title( sprintf( '<h2 class="card-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		</div>
 		<div class="card-footer entry-meta">
 			<?php lean_entry_meta(); ?>
@@ -34,13 +26,13 @@
 </div>
 
 <?php else: ?>
-<div class="col-lg-4">
+<div class="col-lg-3">
 	<div class="card">
 		<a class="entry-img" href="<?php the_permalink(); ?>">
 			<img src="<?php echo THEME_URI;?>/assets/img/placeholder.png" class="card-img-top"/>
 		</a>
 		<div class="card-block">
-			<?php the_title( sprintf( '<h2 class="card-title entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php the_title( sprintf( '<h2 class="card-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 			<?php if ( get_theme_mod( 'posts_list_excerpt')==yes ) { ?>
 				<div class="entry-excerpt hidden-sm-down">
