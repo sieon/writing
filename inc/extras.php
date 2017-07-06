5177658db151a3e9b2052237e24eb82c463d54dc
sieon <sieon@outlook.com>
2017-07-06 22:18:20 +0800

    移除菜单的多余选择器

--- inc/extras.php
+++ inc/extras.php
@@ -87,3 +87,14 @@ if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 	}
 	add_action( 'wp_head', 'lean_render_title' );
 endif;
+
+/**
+ * 移除菜单的多余CSS选择器
+ * From https://www.wpdaxue.com/remove-wordpress-nav-classes.html
+ */
+add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
+add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
+function my_css_attributes_filter($var) {
+	return is_array($var) ? array_intersect($var, array('nav-item','active','dropdown','dropdown-menu')) : '';
+}
