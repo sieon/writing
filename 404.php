195d3796c3213e6497ff0f24c6f5dfd4e19a1a6e
sieon <sieon@outlook.com>
2017-07-06 15:01:43 +0800

    404页面样式修复

--- 404.php
+++ 404.php
@@ -1,7 +1,11 @@
 <?php
 get_header(); ?>
-<div class="container">
-  <h1>404</h1>
-  <p>你要找的不存在。</p>
+<div class="container mt-4">
+  <div class="site-main">
+    <div class="main-content p-3 text-center">
+      <h1 class="display-1 text-muted pt-5 pb-4">404</h1>
+      <p class="display-4 text-muted pb-5">你要找的页面不存在。</p>
+    </div>
+  </div>
 </div>
 <?php get_footer(); ?>
