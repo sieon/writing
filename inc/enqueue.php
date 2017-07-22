ae0a6f1e5bae5f09308cd0b7d5eb350047708fd5
sieon <sieon@outlook.com>
2017-07-22 15:51:40 +0800

    popper.js

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -30,8 +30,8 @@ function lean_scripts() {
 	wp_enqueue_style( 'lean-font-awesome', THEME_URI . '/assets/css/font-awesome.min.css');
 
 	wp_enqueue_script( 'lean-jquery', THEME_URI . '/assets/js/jquery.js', array(), '20170613', true );
-  //wp_enqueue_script( 'lean-popper', THEME_URI . '/assets/js/popper.js', array(), '20170416', true );
-	wp_enqueue_script( 'lean-tether', THEME_URI . '/tether.js', array(), '20170609', true );
+  wp_enqueue_script( 'lean-popper', THEME_URI . '/assets/js/popper.js', array(), '20170416', true );
+	wp_enqueue_script( 'lean-tether', THEME_URI . '/assets/js/tether.js', array(), '20170609', true );
 	wp_enqueue_script( 'lean-bootstrap', THEME_URI . '/assets/js/bootstrap.min.js', array(), '20170616', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
