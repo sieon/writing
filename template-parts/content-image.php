b133f97f3da923f78e8bfb79707e2e480ca42456
sieon <sieon@outlook.com>
2017-07-24 21:36:33 +0800

    添加图片single样式

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -4,21 +4,62 @@
  * @package lean
  */
 ?>
-
 <div class="card card-shadow">
-
-	<a href="<?php //the_permalink(); ?>" class="card-img-top">
-		<?php
-			the_post_thumbnail('full', ['class' => 'card-img-top img-fluid']);
-		?>
-	</a>
-
 	<div class="card-body">
+		<?php if(is_single()): ?>
+
+		<?php the_title( '<h1 class="card-title mb-4">','</h1>' ); ?>
+
+		<p class="card-text text-link-color-muted">
+			<small>
+				<?php lean_entry_meta(); ?>
+			</small>
+		</p>
+
+		<div class="entry-content pt-3">
+
+			<?php
+			if ( get_theme_mod( 'post-tags')==top ) {
+				$posttags = get_the_tags();
+				if ( $posttags ) {
+					echo '<div class="post-tags mb-3">';
+					foreach( $posttags as $tag ) {
+						echo '<a href="' . get_tag_link( $tag->term_id ) . '" class="btn btn-light btn-sm mr-3 mb-2">' . $tag->name . '</a>';
+					}
+					echo '</div>';
+				}
+			}
+			?>
+
+			<?php the_content(); ?>
+
+			<?php
+			if ( get_theme_mod( 'post-tags')==bottom ) {
+				$posttags = get_the_tags();
+				if ( $posttags ) {
+					echo '<div class="post-tags mt-4 mb-3">';
+					foreach( $posttags as $tag ) {
+						echo '<a href="' . get_tag_link( $tag->term_id ) . '" class="btn btn-light btn-sm mr-2 mb-2">' . $tag->name . '</a>';
+					}
+					echo '</div>';
+				}
+			} ?>
+
+		</div>
+
+		<?php else: // not single ?>
+
 		<?php the_title( sprintf( '<h2 class="card-title h5 text-link-color line-clamp-2 text-overflow-ellipsis"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<div class="entry-meta text-link-color-muted">
+		<div class="entry-meta text-link-color-muted mb-3">
 			<small><?php lean_entry_meta2(); ?></small>
 		</div>
+
+		<a href="<?php the_permalink(); ?>" class="card-img">
+			<?php
+				the_post_thumbnail('full', ['class' => 'card-img img-fluid']);
+			?>
+		</a>
 
 		<?php if ( get_theme_mod( 'posts_list_excerpt')==yes ) { ?>
 			<p class="card-text line-clamp-2 text-overflow-ellipsis mt-3 hidden-sm-down">
@@ -40,6 +81,6 @@
 		} ?>
 		</small>
 		</p>
+		<?php endif; // end is_single ?>
 	</div>
-
 </div>
