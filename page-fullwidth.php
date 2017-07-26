f10471768ef1aef8913cacbaa39140a90c4903ec
sieon <sieon@outlook.com>
2017-07-26 13:33:42 +0800

    全宽页面样式

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -6,24 +6,25 @@ get_header(); ?>
 
 <div class="container mt-4">
   <div class="site-main">
-    <main class="main-content card">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <h1 class="card-header bg-white py-4"><?php the_title(); ?></h1>
+    <main class="main-content">
 
-      <div class="card-body">
-        <div class="entry-content">
-          <?php the_content(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="card card-shadow">
+        <h1 class="card-header bg-white py-4"><?php the_title(); ?></h1>
+        <div class="card-body">
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div>
         </div>
       </div>
-        <?php
-          // If comments are open or we have at least one comment, load up the comment template
-          if ( comments_open() || get_comments_number() ) :
-            comments_template();
-          endif;
-        ?>
-        <?php endwhile; ?>
+      <?php
+        // If comments are open or we have at least one comment, load up the comment template
+        if ( comments_open() || get_comments_number() ) :
+          comments_template();
+        endif;
+      ?>
+      <?php endwhile; ?>
       <?php endif; ?>
-
     </main>
 
   </div>
