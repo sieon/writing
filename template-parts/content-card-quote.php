8165f6fc135f45c81125c10b7ab0b638f93a8c84
sieon <sieon@outlook.com>
2017-07-27 20:47:42 +0800

    卡片列表修复

--- template-parts/content-card-quote.php
+++ template-parts/content-card-quote.php
@@ -5,16 +5,13 @@
  */
 ?>
 
-<div class="card card-quote">
-
+<div class="card border-info">
 	<div class="card-body d-flex flex-column justify-content-center">
-
 		<blockquote class="blockquote mb-0">
 			<div class="line-clamp-4 text-overflow-ellipsis">
 				<?php the_content(); ?>
 			</div>
-			<?php the_title( sprintf( '<footer class="blockquote-footer mt-3 line-clamp-2 text-overflow-ellipsis"><cite><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></cite></footer>' ); ?>
+			<?php the_title( sprintf( '<footer class="blockquote-footer mt-3 line-clamp-2 text-overflow-ellipsis text-link-color"><cite><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></cite></footer>' ); ?>
 		</blockquote>
-
 	</div>
 </div>
--- template-parts/content-card.php
+++ template-parts/content-card.php
@@ -19,9 +19,9 @@
 
 	<?php else: // no thumbnail ?>
 
-		<div class="card-body d-flex flex-column justify-content-center">
+		<div class="card-body">
 
-			<?php the_title( sprintf( '<h2 class="card-title h6 text-link-color line-clamp-2 text-overflow-ellipsis"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			<?php the_title( sprintf( '<h2 class="card-title h5 text-link-color line-clamp-2 text-overflow-ellipsis"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 			<p class="card-text entry-excerpt hidden-sm-down line-clamp-4 text-overflow-ellipsis text-muted">
 				<?php echo wp_trim_words( get_the_excerpt(), get_theme_mod( 'excerpt_length'), '...' );?>
@@ -30,7 +30,7 @@
 
 	<?php endif; ?>
 
-	<div class="card-footer text-link-color-muted bg-white border-top-0 pt-0">
+	<div class="card-footer bg-transparent text-link-color-muted">
 		<small class="d-flex justify-content-between">
 			<span><?php the_time(); ?></span>
 			<?php
