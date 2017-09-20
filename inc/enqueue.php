a84cd249b5f4247df919a9941730f188666f6b73
sieon <sieon@outlook.com>
2017-09-20 22:16:52 +0800

    删除主题

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,26 +4,9 @@
  */
 function lean_scripts() {
 
-  switch (get_theme_mod( 'style-colors')) {
-    case 'navbar-dark bg-success':
-    wp_enqueue_style( 'l-toolkit', THEME_URI . '/assets/css/toolkit1.css');
-    break;
-    case 'navbar-dark bg-danger':
-    wp_enqueue_style( 'l-toolkit', THEME_URI . '/assets/css/toolkit2.css');
-    break;
-    case 'navbar-dark bg-warning':
-    wp_enqueue_style( 'l-toolkit', THEME_URI . '/assets/css/toolkit3.css');
-    break;
-    case 'navbar-dark bg-info':
-    wp_enqueue_style( 'l-toolkit', THEME_URI . '/assets/css/toolkit4.css');
-    break;
-    default:
-    wp_enqueue_style( 'l-toolkit', THEME_URI . '/style.min.css');
-    break;
-  }
-
-	wp_enqueue_style( 'l-font-awesome', THEME_URI . '/assets/css/open-iconic-bootstrap.min.css');
-	wp_enqueue_script( 'l-bootstrap', THEME_URI . '/assets/js/scripts.min.js', array('jquery'), '20170616', true );
+  wp_enqueue_style( 'style', THEME_URI . '/style.min.css');
+	wp_enqueue_style( 'iconic', THEME_URI . '/assets/css/open-iconic-bootstrap.min.css');
+	wp_enqueue_script( 'bs4', THEME_URI . '/assets/js/scripts.min.js', array('jquery'), '20170616', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
