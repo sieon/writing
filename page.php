c4e0dc5893e7407ecb45418d0364cd29436cebd7
sieon <sieon@outlook.com>
2017-09-29 17:36:25 +0800

    page entry content add class "clearfix"

--- page.php
+++ page.php
@@ -1,41 +1,44 @@
 <?php get_header(); ?>
 
 <div class="container mt-4">
-  <div class="row">
-    <div class="content-area">
-      <div class="w-100 bg-white border rounded mb-4 p-4 l-shadow">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h1 class="mb-4"><?php the_title(); ?></h1>
-        <hr class="mb-4">
+    <div class="row">
+        <div class="content-area">
+            <div class="w-100 bg-white border rounded mb-4 p-4 l-shadow">
 
-        <div class="entry-content">
-          <?php the_content(); ?>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <h1 class="mb-4"><?php the_title(); ?></h1>
+
+                <hr class="mb-4">
+
+                <div class="entry-content clearfix">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+
+            <?php
+            // If comments are open or we have at least one comment, load up the comment template
+            if ( comments_open() || get_comments_number() ) :
+                comments_template();
+            endif; ?>
+
+            <?php endwhile;?>
+
+            <?php else: ?>
+                <div class="jumbotron">
+                    <h1 class="container">oh,no!</h1>
+                </div>
+                <p>No content is appearing for this page!</p>
+            <?php endif; ?>
         </div>
-      </div>
 
+        <div class="sidebar hidden-md-down">
 
-      <?php
-        // If comments are open or we have at least one comment, load up the comment template
-        if ( comments_open() || get_comments_number() ) :
-          comments_template();
-        endif;
-      ?>
+            <?php if ( !dynamic_sidebar('sidebar-2') ) { _e('','lean'); } ?>
 
-      <?php endwhile;else: ?>
-        <div class="jumbotron">
-          <h1 class="container">oh,no!</h1>
         </div>
-
-        <p>No content is appearing for this page!</p>
-        <?php endif; ?>
 
     </div>
-
-    <div class="sidebar hidden-md-down">
-      <?php if ( !dynamic_sidebar('sidebar-2') ) { _e('','lean'); } ?>
-    </div>
-
-  </div>
 </div><!--./container -->
 
 <?php get_footer(); ?>
